f0c13eabb248fd11bd81edb0fda626f1ff3164c6
Mb4y4 <d.sommy@gmail.com>
2023-11-01 19:50:57 +0300

    first commit

---
+++ day1_challenge.py
@@ -0,0 +1,11 @@
+buying = float(input("Enter initial cost: "))
+selling = float(input("Enter selling price: "))
+units = float(input("Enter no of units sold: "))
+
+# print((selling_price - cost_of_item)* no_of_units)
+
+def profit_computation(buying, selling, units): # function declaration
+    #function definition
+    profit = (selling-buying)*units
+    return profit
+print(profit_computation(buying, selling, units)) # function call
