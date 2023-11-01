0bf55bef4c5dc616933533bd6d0b885a0b50d3ba
Mb4y4 <d.sommy@gmail.com>
2023-11-01 21:51:14 +0300

    day-2

---
+++ day2_challenge.py
@@ -0,0 +1,18 @@
+#currency calculator
+
+number1 = float(input('Enter first number: '))
+number2 = float(input('Enter second number: '))
+operator = input('Enter operator: ')
+
+if operator == '+':
+    print (number1 + number2)
+elif operator == '-':
+    print (number1 - number2)
+elif operator == '*':
+    print (number1 * number2)
+elif operator == '/':
+    print (number1 / number2)
+elif operator == '%':
+    print (number1 % number2)
+else :
+    print('Operation not found')
