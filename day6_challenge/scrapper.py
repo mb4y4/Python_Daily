3f80a938b09b12834d8a2500738492b19998c839
Mb4y4 <d.sommy@gmail.com>
2023-11-08 18:56:01 +0300

    scrapping wikipedia page

---
+++ day6_challenge/scrapper.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://en.wikipedia.org/wiki/Web_scraping'
+html = requests.get(url)
+
+# print if connection is successful or not
+#print(html)
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+
+ #create a new html file to insert the content
+    with open('scrapper.html', 'w', encoding='utf-8') as html_file:
+        html_file.write(soup.prettify())
+    #finding the container wrapping the content we want
+    container = soup.find('div', class_='mw-page-container-inner')
+    article_titles = container.find_all('div', class_='mw-parser-output')
+    with open('scrapper.txt', 'w', encoding='utf-8') as titles:
+        for title in article_titles:
+            texts = title.text
+            titles.write(f"{texts} \n")
+
+else:
+    print(f"Failed to retrieve the page. Status code: {response.status_code}")
