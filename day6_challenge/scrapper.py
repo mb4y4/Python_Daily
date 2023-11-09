6d7d7fdea40001a783637fa200289a81c3a5e0b0
Mb4y4 <d.sommy@gmail.com>
2023-11-09 10:46:36 +0300

    2nd commit

--- day6_challenge/scrapper.py
+++ day6_challenge/scrapper.py
@@ -18,12 +18,13 @@ if response.status_code == 200:
     with open('scrapper.html', 'w', encoding='utf-8') as html_file:
         html_file.write(soup.prettify())
     #finding the container wrapping the content we want
-    container = soup.find('div', class_='mw-page-container-inner')
-    article_titles = container.find_all('div', class_='mw-parser-output')
-    with open('scrapper.txt', 'w', encoding='utf-8') as titles:
-        for title in article_titles:
-            texts = title.text
-            titles.write(f"{texts} \n")
+    #container = soup.find('div', class_='mw-page-container-inner')
+    article_titles = soup.find('div', class_='mw-parser-output')
+    # with open('scrapper.txt', 'w', encoding='utf-8') as titles:
+    #     for title in article_titles:
+    #         texts = title.text
+    #         titles.write(f"{texts} \n")
+    print (article_titles)
 
 else:
     print(f"Failed to retrieve the page. Status code: {response.status_code}")
